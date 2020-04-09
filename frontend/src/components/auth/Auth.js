9eb6da0d874c25324a2ef43d16312f6eeb74914f
Nil Quera <nlquera97@gmail.com>
2020-04-09 17:59:29 +0200

    login modifica context autentificació

--- frontend/src/components/auth/Auth.js
+++ frontend/src/components/auth/Auth.js
@@ -1,19 +1,58 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 
 export const AuthContext = React.createContext();
 
 const Auth = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+  const [loadingUser, setLoadingUser] = useState(true);
 
   useEffect(() => {
     setIsAuthenticated(false);
   }, []);
 
-  return (
-    <AuthContext.Provider value={{ isAuthenticated }}>
-      {children}
-    </AuthContext.Provider>
-  );
+  async function login(email, password) {
+    const userData = {
+      user: "Nil Quera",
+      email: "nlquera97@gmail.com"
+    };
+
+    if (email === "nlquera97@gmail.com") {
+      setUser(userData);
+      setIsAuthenticated(true);
+    }
+
+    console.log(user);
+    console.log(isAuthenticated);
+  }
+
+  async function signup(email, password) {
+    const userData = {
+      user: "Nou Usuari",
+      email: "noucorreu@gmail.com"
+    };
+
+    setUser(userData);
+    setIsAuthenticated(true);
+  }
+
+  function logout() {
+    setUser(null);
+    setIsAuthenticated(false);
+  }
+
+  const value = React.useMemo(() => {
+    return {
+      user,
+      isAuthenticated,
+      loadingUser,
+      login,
+      signup,
+      logout
+    };
+  }, [user, loadingUser, isAuthenticated]);
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
 export default Auth;
--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,48 +1,71 @@
-import React, { Component } from "react";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "./Auth";
+import { Redirect } from "react-router-dom";
 
-class Login extends Component {
-  render() {
-    return (
-      <form>
-        <h3>Sign In</h3>
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirectToHome, setRedirectToHome] = useState(false);
+  const { login } = useContext(AuthContext);
 
-        <div className="form-group">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-          />
-        </div>
-
-        <div className="form-group">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-          />
-        </div>
-
-        <div className="form-group">
-          <div className="custom-control custom-checkbox">
-            <input
-              type="checkbox"
-              className="custom-control-input"
-              id="customCheck1"
-            />
-            <label className="custom-control-label" htmlFor="customCheck1">
-              Remember me
-            </label>
-          </div>
-        </div>
-
-        <button type="submit" className="btn btn-primary btn-block">
-          Submit
-        </button>
-      </form>
-    );
+  function handleChange(event) {
+    // console.log(email, password);
+    event.target.type === "email"
+      ? setEmail(event.target.value)
+      : setPassword(event.target.value);
   }
-}
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    login(email, password);
+    // setRedirectToHome(true);
+    // console.log(email, password);
+  }
+
+  if (redirectToHome) return <Redirect to={"/"} />;
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>Sign In</h3>
+
+      <div className="form-group">
+        <label>Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Enter email"
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Enter password"
+          onChange={handleChange}
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary btn-block">
+        Submit
+      </button>
+    </form>
+  );
+};
 
 export default Login;
+
+// <div className="form-group">
+//   <div className="custom-control custom-checkbox">
+//     <input
+//       type="checkbox"
+//       className="custom-control-input"
+//       id="customCheck1"
+//       />
+//     <label className="custom-control-label" htmlFor="customCheck1">
+//       Remember me
+//     </label>
+//   </div>
+// </div>
