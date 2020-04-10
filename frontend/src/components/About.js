816d108f82ac216508fdbda13c8a30fb52fb35a7
Nil Quera <nlquera97@gmail.com>
2020-04-10 14:32:21 +0200

    Actualitza about

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,36 +1,60 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 function About() {
-  useEffect(() => {
-    fetchItems();
-  }, []);
-
-  const [items, setItems] = useState([]);
-
-  const fetchItems = async () => {
-    const data = [
-      {
-        id: 1,
-        nom: "Nil"
-      },
-      {
-        id: 2,
-        nom: "Pol"
-      },
-      {
-        id: 3,
-        nom: "Maria"
-      }
-    ];
-    console.log(data);
-    setItems(data);
-  };
-
   return (
     <div>
-      {items.map(item => {
-        return <h1 key={item.id}>{item.nom}</h1>;
-      })}
+      <h1>Com funciona?</h1>
+      <p>
+        <i>
+          Durant el confinament per la pandèmia del COVID-19, molts venedors i
+          venedores dels nostres barris han vist com disminuien les ventes de
+          forma preocupant.
+        </i>
+      </p>
+      <p>
+        <b>Comencia</b> és una plataforma molt senzilla destinada a facilitar el
+        comerç a les botigues de barri.
+      </p>
+      <p>
+        El venedor crea un <b>anunci</b> per la seva botiga i afegeix una sèrie
+        de <b>lots de productes</b> disponibles.
+      </p>
+      <p>
+        Mitjançant un <b>mapa interactiu</b>, els veins i veines de la zona
+        poden trobar els anuncis dels comerços més propers que necessiten ajuda.
+      </p>
+      <h2 style={{ color: "#88B04B" }}>Si ets venedor:</h2>
+      <ol>
+        <li>
+          <Link to="/login">Accedeix</Link> o{" "}
+          <Link to="/signup">Registra't</Link>
+        </li>
+        <p>Necessitarem un telèfon per validar el teu negoci</p>
+        <li>Afegeix un anunci</li>
+        <p>
+          Et demanarem que introdueixis els lots de productes que ofereix el teu
+          comerç
+        </p>
+      </ol>
+      <h2 style={{ color: "#FF6F61" }}>Si ets comprador:</h2>
+      <ol>
+        <li>
+          <Link to="/login">Accedeix</Link> o{" "}
+          <Link to="/signup">Registra't</Link>
+        </li>
+        <li>
+          Troba els comerços del teu barri al{" "}
+          <Link to="/">mapa interactiu</Link>
+        </li>{" "}
+        <li>
+          Selecciona un anunci i escull el lot que t'interessa.{" "}
+          <u>
+            Et posarem en contacte amb la botiga perquè podeu efectuar la
+            compra.
+          </u>
+        </li>
+      </ol>
     </div>
   );
 }
